e5ac3a9a4354e7953a52f27270e9fe2e30520db7
amytzeng <tzengamy@gmail.com>
2025-12-22 19:28:54 +0800

    test 1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res
     .status(200)
-    .send("<h1>Welcome to the CI/CD Workshop! - Staging Environment</h1>");
+    .send("<h1>Welcome to the CI/CD Workshop! - test 1</h1>");
 });
 
 // Add time endpoint 新增這裡
